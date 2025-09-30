c47496b8bef1630b381d01b193cbc1301be8d024
sasimopuri <sasikmopuriu@gmail.com>
2025-10-01 01:33:57 +0530

    Added BrowserRouter

--- devtinder-ui/src/App.jsx
+++ devtinder-ui/src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Body from "./Body";
+
 function App() {
   return (
     <>
-      <h1>Hello Dev</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Body />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
