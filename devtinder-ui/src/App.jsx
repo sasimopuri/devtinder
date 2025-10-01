4c211fdc01a02d25c30bc61d0b402e4fd8090f3f
sasimopuri <sasikmopuriu@gmail.com>
2025-10-01 14:53:07 +0530

    Configured Redux tool kit and created userSlice

--- devtinder-ui/src/App.jsx
+++ devtinder-ui/src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignUp from "./SignUp";
-import Login from "./Login";
-import Body from "./Body";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
-    <>
+    <Provider store={appStore}>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<Body />}></Route>
+          <Route path="/" element={<Body />}>
+            
+          </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
