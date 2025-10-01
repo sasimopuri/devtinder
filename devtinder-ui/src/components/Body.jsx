414ba3d2a914ebd43ed70d530e8b2b6f5bf51bac
sasimopuri <sasikmopuriu@gmail.com>
2025-10-01 15:54:51 +0530

    Added Route Auth

--- devtinder-ui/src/components/Body.jsx
+++ devtinder-ui/src/components/Body.jsx
@@ -1,7 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Nav from "./Nav";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const getUserDetails = async () => {
+    try {
+      const response = await axios.get(BASE_URL + "profile/getProfile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(response?.data));
+    } catch (err) {
+      navigate("/login");
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    if (!user) {
+      getUserDetails();
+    }
+  }, []);
+  if (!user) {
+    return;
+  }
   return (
     <>
       <Nav />
@@ -10,4 +37,4 @@ const Body = () => {
   );
 };
 
-export default Body
+export default Body;
