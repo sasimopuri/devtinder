e5dbc291f68e970705a3a9de58aff6fc484b3977
sasimopuri <sasikmopuriu@gmail.com>
2025-10-02 17:16:07 +0530

    Removed logs

--- devtinder-ui/src/components/Nav.jsx
+++ devtinder-ui/src/components/Nav.jsx
@@ -9,7 +9,6 @@ const Nav = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  console.log("user", user);
   async function handleLogout() {
     try {
       await axios.get(BASE_URL + "/logout", { withCredentials: true });
