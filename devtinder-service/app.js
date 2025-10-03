e92eb892df0cd28e54e6c7ad632c79684a2ba164
sasimopuri <sasikmopuriu@gmail.com>
2025-10-03 20:25:33 +0530

    Added cors config

--- devtinder-service/app.js
+++ devtinder-service/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./src/config/database");
 const Users = require("./src/models/userSchema");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { authRouter } = require("./src/routes/auth");
 const { profileRouter } = require("./src/routes/profile");
@@ -12,12 +13,16 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/", authRouter);
 app.use("/profile", profileRouter);
 app.use("/request", requestRouter);
 app.use("/user", userRouter);
-
-
 
 app.delete("/deleteById", async (req, res) => {
   const userId = req.body.userId;
@@ -37,7 +42,7 @@ app.delete("/deleteById", async (req, res) => {
 app.patch("/updateUser/:userId", async (req, res) => {
   const userId = req.params?.userId;
   const body = req?.body;
-  const notAllowedFeilds = ["email"];
+  const notAllowedFeilds = ["email","age","gender"];
   try {
     if (!body) {
       throw new Error("No Parameters found");
