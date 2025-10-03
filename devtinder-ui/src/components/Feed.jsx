1f114e9956e59428c254f20058636c7d559091fc
sasimopuri <sasikmopuriu@gmail.com>
2025-10-03 18:02:12 +0530

    Developed Feed Page logic

--- devtinder-ui/src/components/Feed.jsx
+++ devtinder-ui/src/components/Feed.jsx
@@ -1,37 +1,81 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { addFeed } from "../utils/feedSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL, DEFAULT_USER_IMG } from "../utils/constants";
 const Feed = () => {
   const dispatch = useDispatch();
-  const [feedData, setFeedData] = useState();
+  const feedDetails = useSelector((state) => state.feed);
+  const feedData = feedDetails?.allUsers || [];
+  const count = feedDetails?.count || 0;
+  const [outOfUsers, setOutOfUsers] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const fetchFeed = async () => {
     try {
-      const response = await axios.get(BASE_URL + "/user/feed/1/10", {
-        withCredentials: true,
-      });
-      dispatch(addFeed(response?.data));
-      setFeedData(response?.data);
+      console.log("inside fet");
+
+      const response = await axios.get(
+        BASE_URL + `/user/feed/${currentIndex === 0 ? 1 : 2}/5`,
+        {
+          withCredentials: true,
+        }
+      );
+      const data = response?.data;
+      dispatch(
+        addFeed({
+          count: count + data?.count,
+          allUsers: [...feedData, ...data?.allUsers],
+        })
+      );
+      if (data?.count === 0) {
+        setOutOfUsers(true);
+      }
       console.log(response?.data);
     } catch (err) {
       console.log(err);
     }
   };
   useEffect(() => {
-    fetchFeed();
-  }, []);
-  const userData = feedData && feedData?.allUsers[0];
+    if ((!outOfUsers && !feedData) || feedData?.length - currentIndex < 2) {
+      fetchFeed();
+    }
+  }, [currentIndex, outOfUsers]);
+
+  const handleRequest = async (type) => {
+    try {
+      await axios.post(
+        BASE_URL + `/request/send/${type}/${feedData[currentIndex]._id}`,
+        {},
+        { withCredentials: true }
+      );
+      setCurrentIndex((prevState) => prevState + 1);
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+
+  if (!feedData) {
+    return;
+  }
+
+  const userData = feedData[currentIndex];
+  console.log("FeedData", userData);
+  if (!userData) {
+    return (
+      <div className="text-center mt-10">
+        No more profiles to show right now! 😊
+      </div>
+    );
+  }
   return (
     <div className="flex justify-center mt-6">
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure>
           <img
             src={
-              feedData?.allUsers &&
+              userData &&
               (userData.photoUrl ? userData.photoUrl : DEFAULT_USER_IMG)
             }
-            // src={feedData?.allUsers[1]?.imageUrl} why this is not working?
             alt="Pic"
           />
         </figure>
@@ -49,7 +93,8 @@ const Feed = () => {
             </p>
             <p>
               <span className="font-semibold">Gender: </span>
-              {userData?.gender[0].toUpperCase() + userData?.gender.slice(1)}
+              {userData?.gender &&
+                userData?.gender[0]?.toUpperCase() + userData?.gender.slice(1)}
             </p>
           </div>
           <p>
@@ -62,8 +107,16 @@ const Feed = () => {
           </p>
 
           <div className="card-actions justify-center">
-            <button className="btn bg-red-200 rounded-xl">Ignore</button>
-            <button className="btn bg-green-200 rounded-xl">
+            <button
+              className="btn bg-red-200 rounded-xl"
+              onClick={() => handleRequest("ignored")}
+            >
+              Ignore
+            </button>
+            <button
+              className="btn bg-green-200 rounded-xl"
+              onClick={() => handleRequest("interested")}
+            >
               Send Request{" "}
             </button>
           </div>
